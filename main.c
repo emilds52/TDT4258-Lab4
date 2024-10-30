b5eed0a6dac65c5588c8f1e8ba9f12b7335f3a68
emilds@ntnu.no <emilds@ntnu.no>
2024-10-30 10:11:26 +0000

    Polling (did not beat target)
    Signed-off-by: emilds@ntnu.no <emilds@ntnu.no>

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <xc.h>
 #include <stdio.h>
-// #include "usart.h"
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+#include "usart.h"
 
 // 256*100/1024
 #define DACREF_VALUE 25
@@ -14,6 +16,7 @@ void AC_init(){
     AC0.MUXCTRL = AC_MUXPOS_AINP0_gc |
                   AC_MUXNEG_DACREF_gc;
     AC0.CTRLA = AC_POWER_PROFILE2_gc |
+                AC_RUNSTDBY_bm |
                 AC_ENABLE_bm;
     AC0.DACREF = DACREF_VALUE;
     AC0.CTRLB = AC_WINSEL_DISABLED_gc;
@@ -36,26 +39,50 @@ void set_LED_off(){
 }
 
 
-void main(void) {
+// Function to initialize sleep mode
+void sleep_init(void) {
+    set_sleep_mode(SLEEP_MODE_STANDBY);  // Standby sleep mode
+}
+
+
+void TCA0_init() {
+    // Set the period of the timer. PER = period[s] * F_CPU / Prescaler = 0.01s * 4 000 000 Hz / 2
+    TCA0.SINGLE.PER = 20000;
+    // Enable timer overflow interrupt
+    TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
+    // Run timer in standby mode, set prescaler to 2, enable timer
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_RUNSTDBY_bm | TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm;
+}
+
+ISR(TCA0_OVF_vect)
+{
+
+    // Add your code here 
+    if (AC0.STATUS & AC_CMPSTATE_bm){
+        set_LED_off();
+        //char message[100];
+        //sprintf(message, "0x%02x \n", AC0.STATUS);
+        //USART3_SendString(message);
+    } else {
+        set_LED_on();
+    }
+    // Clear interrupt flag
+    TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
+}
+
+int main(){
     AC_init();
     VREF_init();
     LED_init();
     //USART3_Init();
-    //char message[100];
-    //sprintf(message, "MUXCTRL: 0x%02x\n", AC0.MUXCTRL);
-    //USART3_SendString(message);
-    //sprintf(message, "CTRLA: 0x%02x\n", AC0.CTRLA);
-    //USART3_SendString(message);
-    //sprintf(message, "DACREF: 0x%02x\n", AC0.DACREF);
-    //USART3_SendString(message);
-    while(1){
-        if (AC0.STATUS & AC_CMPSTATE_bm){
-            set_LED_off();
-            //sprintf(message, "0x%02x\n", AC0.STATUS);
-            //USART3_SendString(message);
-        } else {
-            set_LED_on();
-        }
+    // Initialize timer
+    TCA0_init();
+    // Enable interrupts
+    sei();
+    sleep_init();
+
+    while(1) {
+        sleep_mode();
     }
-    return;
-}
+    return 0;
+};
